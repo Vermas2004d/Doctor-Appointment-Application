17e3c17363c77a7c11a9239d398c1c299149dfef
Vermas2004d <madhavverma971@gmail.com>
2025-10-28 00:23:55 +0530

    intial commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html",   
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#5f6FFF',
+      }
+    },
+  },
+  plugins: [],
+}
+
