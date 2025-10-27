13e3ccc5813c4c167a9d0b2ac0754a0c8403719c
Vermas2004d <madhavverma971@gmail.com>
2025-10-28 02:51:34 +0530

    adding files

---
+++ frontend/src/components/Banner.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+        {/* ---left side  */}
+        <div>
+            <div>
+                <p>Book Appointment</p>
+                <p>With 100+ Trusted Doctors</p>
+            </div>
+
+        </div>
+
+        {/* ----right side */}
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Banner
